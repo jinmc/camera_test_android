a416fa67e1f7fc7e8bc42a49c47e4d221e48d6d8
jinmo chong <jinmo.chong@geappliances.com>
2025-01-06 11:44:16 +0900

    temp check

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,98 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var capReq: CaptureRequest.Builder
+    lateinit var handler: Handler
+    lateinit var handlerThread: HandlerThread
+    lateinit var cameraManager: CameraManager
+    lateinit var textureView: TextureView
+    lateinit var cameraCaptueSession: CameraCaptureSession
+    lateinit var cameraDevice: CameraDevice
+    lateinit var captureRequest: CaptureRequest
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         get_permissions()
+
+        textureView = findViewById(R.id.textureView)
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+
+        textureView.surfaceTextureListener = object: TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return true
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+            }
+
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    fun open_camera() {
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object: CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
+                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                var surface = Surface(textureView.surfaceTexture)
+                capReq.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface), object: CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        cameraCaptueSession = session
+                        captureRequest = capReq.build()
+                        cameraCaptueSession.setRepeatingRequest(captureRequest, null, handler)
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                        TODO("Not yet implemented")
+                    }
+                }, handler)
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                cameraDevice.close()
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+                cameraDevice.close()
+            }
+        }, handler)
     }
 
     fun get_permissions() {
